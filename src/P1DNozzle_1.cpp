58cd31750d6752b801a958c05abd3f05694301b5
Qi Zhitong <qizhitong@QideMacBook-Air.local>
2026-05-01 21:00:32 +0800

    init

---
+++ src/P1DNozzle_1.cpp
@@ -0,0 +1,128 @@
+//亚声速-超声速等熵喷管(非守恒型)
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<iomanip>
+#include<string>
+#include <fstream>
+
+using namespace std;
+
+constexpr double gamma = 1.4;
+constexpr double R = 287.0;
+constexpr double cfl = 0.5;
+constexpr double deltaX = 0.1;
+constexpr int cellNum = 30;
+constexpr int timesteps = 1400;
+
+void exportToFile(const string& filename,double X[],double A[],double rho[],
+                  double V[],double T[],double p[],double Ma[],
+                  double mdot[],int cellNum) {
+    ofstream fout(filename);
+    if (!fout) {
+        cerr << "无法创建文件: " << filename << endl;
+        return;
+    }
+
+    fout << fixed << setprecision(3);
+
+    fout << "No" << ',' << "X" << ',' << "A" << ',' << "rho" << ','
+         << "V" << ',' << "T" << ',' << "p" << ',' 
+         << "Ma" << ',' <<"mdot" <<endl;
+    for (int i = 0; i < cellNum + 1; i++) {
+        fout << i + 1 << ','
+             << X[i] << ',' << A[i] << ',' << rho[i] << ','
+             << V[i] << ',' << T[i] << ',' << p[i] << ',' 
+             << Ma[i] <<',' <<mdot[i] << endl;
+    }
+}
+
+int main(){
+    double X[cellNum+1],A[cellNum+1],rho[cellNum+1],V[cellNum+1],T[cellNum+1],
+    p[cellNum+1],Ma[cellNum+1],mdot[cellNum+1];
+    double drhodt[cellNum+1],dVdt[cellNum+1],dTdt[cellNum+1];
+    double rhop[cellNum+1],Vp[cellNum+1],Tp[cellNum+1];
+    double drhopdt[cellNum+1],dVpdt[cellNum+1],dTpdt[cellNum+1];
+    double drhodtav[cellNum+1],dVdtav[cellNum+1],dTdtav[cellNum+1];
+    double deltaT = 1.00,pdeltaT = 1.00;
+
+    //给定初值条件
+    for(int i=0;i<cellNum+1;i++){
+        X[i] = i * deltaX;
+        A[i] = 1.0 + 2.2 * (X[i]-1.5)*(X[i]-1.5);
+        rho[i] = 1.0 - 0.314 * X[i];
+        T[i] = 1.0 - 0.2314 * X[i];
+        V[i] = (0.1 + 1.09 * X[i])*sqrt(T[i]);
+        Ma[i] = V[i]/sqrt(T[i]);
+        mdot[i] = rho[i] * V[i] * A[i];
+    }
+
+    exportToFile("../output/Q1_initval.csv",X,A,rho,V,T,p,Ma,mdot,cellNum);
+
+    for (int j=0;j<timesteps;j++){
+    //Maccormack
+    //确定时间步长
+    for(int i = 0;i < cellNum+1; i++){
+        pdeltaT = (cfl * deltaX)/(V[i]+sqrt(T[i]));
+        if(deltaT>pdeltaT)
+        deltaT=pdeltaT;
+    }
+
+    //预估步
+    for (int i = 0; i < cellNum; i++){
+	drhodt[i] = -V[i] * (rho[i + 1] - rho[i]) / deltaX
+		- rho[i] * (V[i + 1] - V[i]) / deltaX
+		- rho[i] * V[i] * (log(A[i + 1]) - log(A[i])) / deltaX;//7-51,p215
+	dVdt[i] = -V[i] * (V[i + 1] - V[i]) / deltaX
+		- (1 / gamma) * ((T[i + 1] - T[i]) / deltaX + T[i] / rho[i] * (rho[i + 1] - rho[i]) / deltaX);//7-52
+	dTdt[i] = -V[i] * (T[i + 1] - T[i]) / deltaX
+		- (gamma - 1) * T[i] * ((V[i + 1] - V[i]) / deltaX + V[i] * (log(A[i + 1]) - log(A[i])) / deltaX);//7-53
+	rhop[i] = rho[i] + drhodt[i] * deltaT;//7-54,p215
+	Vp[i] = V[i] + dVdt[i] * deltaT;//7-55
+	Tp[i] = T[i] + dTdt[i] * deltaT;//7-56
+    }
+
+    //校正步
+    for (int i = 1; i < cellNum;i++){
+	drhopdt[i] = -Vp[i] * (rhop[i] - rhop[i-1]) / deltaX
+		- rhop[i] * (Vp[i] - Vp[i - 1]) / deltaX
+		- rhop[i] * Vp[i] * (log(A[i]) - log(A[i - 1])) / deltaX;//7-57,p216
+	dVpdt[i] = -Vp[i] * (Vp[i] - Vp[i - 1]) / deltaX
+		- (1 / gamma) * ((Tp[i] - Tp[i - 1]) / deltaX + Tp[i] / rhop[i] * (rhop[i] - rhop[i - 1]) / deltaX);//7-58
+	dTpdt[i] = -Vp[i] * (Tp[i] - Tp[i - 1]) / deltaX
+		- (gamma - 1) * Tp[i] * ((Vp[i] - Vp[i - 1]) / deltaX + Vp[i] * (log(A[i]) - log(A[i - 1])) / deltaX);//7-59
+	drhodtav[i] = (drhodt[i] +drhopdt[i])/2;//7-60,p215
+	dVdtav[i] = (dVdt[i] +dVpdt[i])/2;//7-61
+	dTdtav[i] = (dTdt[i] +dTpdt[i])/2;//7-62
+	rho[i] += drhodtav[i] * deltaT;//7-63
+	V[i] += dVdtav[i] * deltaT;//7-64
+	T[i] += dTdtav[i] * deltaT;//7-65
+    }
+    for(int i=1;i < cellNum; i++){
+        p[i] = rho[i] * T[i];
+        Ma[i] = V[i]/sqrt(T[i]);
+        mdot[i] = rho[i] * V[i] * A[i];
+    }
+
+    //头尾部
+    V[0] = 2 * V[1] - V[2];
+    p[0] = rho[0] * T[0];
+    Ma[0] = V[0]/sqrt(T[0]);
+    mdot[0] = rho[0] * V[0] * A[0];
+    V[cellNum] = 2 * V[cellNum-1] - V[cellNum-2];
+    rho[cellNum] = 2 * rho[cellNum-1] - rho[cellNum-2];
+    T[cellNum] = 2 * T[cellNum-1] - T[cellNum-2];
+    p[cellNum] = rho[cellNum] * T[cellNum];
+    Ma[cellNum] = V[cellNum]/sqrt(T[cellNum]);
+    mdot[cellNum] = rho[cellNum] * V[cellNum] * A[cellNum];
+
+    if(j==0){
+       exportToFile("../output/Q1_iteration1.csv",X,A,rho,V,T,p,Ma,mdot,cellNum);
+    }
+    }
+
+    //输出timesteps步后的数据
+    exportToFile("../output/Q1_final.csv",X,A,rho,V,T,p,Ma,mdot,cellNum);
+
+    return 0;
+}
---
+++ src/P1DNozzle_3.cpp
@@ -0,0 +1,189 @@
+//亚声速-超声速等熵喷管(守恒型,激波捕捉)
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<iomanip>
+#include<string>
+#include <fstream>
+
+using namespace std;
+
+constexpr double gamma = 1.400;
+constexpr double R = 287.000;
+constexpr double cfl = 0.500;
+constexpr double deltaX = 0.100;
+constexpr int cellNum = 30;
+constexpr int timesteps = 1600;
+constexpr double Cx = 0.200;
+
+void exportToFile(const string& filename,double X[],double A[],double rho[],
+                  double V[],double T[],double p[],double Ma[],
+                  double mdot[],double U1[],double U2[],double U3[],int cellNum) {
+    ofstream fout(filename);
+    if (!fout) {
+        cerr << "无法创建文件: " << filename << endl;
+        return;
+    }
+
+    fout << fixed << setprecision(3);
+
+    fout << "No" << '\t' << "X" << '\t' << "A" << '\t' << "rho" << '\t'
+         << "V" << '\t' << "T" << '\t' << "p" << '\t'
+         << "Ma" << '\t' <<"mdot" << '\t' << "U1" << '\t' << "U2" << '\t'
+         << "U3" << endl;
+    for (int i = 0; i < cellNum + 1; i++) {
+        fout << i + 1 << '\t'
+             << X[i] << '\t' << A[i] << '\t' << rho[i] << '\t'
+             << V[i] << '\t' << T[i] << '\t' << p[i] << '\t' 
+             << Ma[i] <<'\t' <<mdot[i] << '\t' << U1[i] << '\t' << U2[i] <<'\t'
+             << U3[i] <<endl;
+    }
+}
+
+double F1(double U2){
+    return U2;
+}
+
+double F2(double U1,double U2,double U3){
+    double res;
+    res = ((U2 * U2)/U1) + ((gamma - 1.0)/gamma)*(U3-((0.5 * gamma * U2 * U2)/U1));
+    return res;
+}
+
+double F3(double U1,double U2,double U3){
+    double res;
+    res = (gamma*U2*U3/U1)-((gamma*(gamma-1.0)*U2*U2*U2)/(2*U1*U1));
+    return res;
+}
+
+double Tcal(double U1,double U2,double U3){
+    return (gamma-1.0) * ((U3/U1)-(( 0.5 * gamma * U2 *U2)/(U1*U1)));
+}
+
+//rho = U1/A
+//J2与A有关，此处不给出
+
+int main(){
+    double U1[cellNum+1],U2[cellNum+1],U3[cellNum+1],pU1[cellNum+1],pU2[cellNum+1],pU3[cellNum+1];
+    double X[cellNum+1],A[cellNum+1],rho[cellNum+1],V[cellNum+1],T[cellNum+1],
+    p[cellNum+1],Ma[cellNum+1],mdot[cellNum+1],prho[cellNum+1],pT[cellNum+1],pp[cellNum+1];
+    double dU1dt[cellNum+1] ,dU2dt[cellNum+1] ,dU3dt[cellNum+1] ,
+    pdU1dt[cellNum+1] ,pdU2dt[cellNum+1] ,pdU3dt[cellNum+1],
+    dU1dtav[cellNum+1] ,dU2dtav[cellNum+1] ,dU3dtav[cellNum+1];
+    double deltaT = 1e6 ,pdeltaT = 1.00;
+
+    //给定初值条件
+    for(int i=0;i<cellNum+1;i++){
+        X[i] = i * deltaX;
+        A[i] = 1.0 + 2.2 * (X[i]-1.5)*(X[i]-1.5);
+        if(i<=5){
+            rho[i] = 1.0;
+            T[i] = 1.0;
+        }
+        else if(i>=6 && i<=15){
+            rho[i] = 1.0 - 0.366 * (X[i] - 0.5);
+            T[i] = 1.0 - 0.167 * (X[i] - 0.5);
+        }
+        else if(i>=16 && i<=20){
+            rho[i] = 0.634 - 0.702 * (X[i]-1.5);
+            T[i] = 0.833 - 0.4908 * (X[i]-1.5);
+        }
+        else{
+            rho[i] = 0.5892 - 0.10228 * (X[i]-2.1);
+            T[i] = 0.93968 - 0.0622 * (X[i]-2.1);
+        }
+        V[i] = 0.59/(rho[i]*A[i]);
+        Ma[i] = V[i]/sqrt(T[i]);
+        mdot[i] = 0.59;
+        p[i] = rho[i]*T[i];
+        U1[i] = rho[i]*A[i];
+        U2[i] = 0.59;
+        U3[i] = rho[i]*A[i]*((T[i]/(gamma-1))+(gamma*V[i]*V[i]/2));
+    }
+
+    exportToFile("../output/Q3_initval.csv",X,A,rho,V,T,p,Ma,mdot,U1,U2,U3,cellNum);
+
+    for (int j=0;j<timesteps;j++){
+    //Maccormack
+    //确定时间步长
+    deltaT = 1e6;
+    for(int i = 0;i < cellNum+1; i++){
+        pdeltaT = (cfl * deltaX)/(V[i]+sqrt(T[i]));
+        if(deltaT>pdeltaT)
+        deltaT=pdeltaT;
+    }
+
+    //预估步
+    for (int i = 0; i < cellNum; i++){
+	dU1dt[i] = -(F1(U2[i+1])-F1(U2[i]))/deltaX;
+    dU2dt[i] = -(F2(U1[i+1],U2[i+1],U3[i+1])-F2(U1[i],U2[i],U3[i]))/deltaX
+    + rho[i] * T[i] * (A[i+1]-A[i])/(gamma * deltaX);
+    dU3dt[i] = -(F3(U1[i+1],U2[i+1],U3[i+1])-F3(U1[i],U2[i],U3[i]))/deltaX;
+    //人工粘性
+    if(i==0){
+        pU1[i] = U1[i] + dU1dt[i] * deltaT;
+        pU2[i] = U2[i] + dU2dt[i] * deltaT;
+        pU3[i] = U3[i] + dU3dt[i] * deltaT;
+    }
+    else{
+        pU1[i] = U1[i] + dU1dt[i] * deltaT + (Cx * abs(p[i+1]-2*p[i]+p[i-1]) * (U1[i+1]-2*U1[i]+U1[i-1])/(p[i+1]+2*p[i]+p[i-1]));
+        pU2[i] = U2[i] + dU2dt[i] * deltaT + (Cx * abs(p[i+1]-2*p[i]+p[i-1]) * (U2[i+1]-2*U2[i]+U2[i-1])/(p[i+1]+2*p[i]+p[i-1]));
+        pU3[i] = U3[i] + dU3dt[i] * deltaT + (Cx * abs(p[i+1]-2*p[i]+p[i-1]) * (U3[i+1]-2*U3[i]+U3[i-1])/(p[i+1]+2*p[i]+p[i-1]));
+    }
+    prho[i] = pU1[i]/A[i];
+    pT[i] = Tcal(pU1[i],pU2[i],pU3[i]);
+    pp[i] = prho[i]*pT[i];
+    }
+
+    //校正步
+    for (int i = 1; i < cellNum;i++){
+	pdU1dt[i] = -(F1(pU2[i])-F1(pU2[i-1]))/deltaX;
+    pdU2dt[i] = -(F2(pU1[i],pU2[i],pU3[i])-F2(pU1[i-1],pU2[i-1],pU3[i-1]))/deltaX
+    + prho[i] * pT[i] * (A[i]-A[i-1])/(gamma * deltaX);
+    pdU3dt[i] = -(F3(pU1[i],pU2[i],pU3[i])-F3(pU1[i-1],pU2[i-1],pU3[i-1]))/deltaX;
+    dU1dtav[i] = (dU1dt[i]+pdU1dt[i])/2;
+    dU2dtav[i] = (dU2dt[i]+pdU2dt[i])/2;
+    dU3dtav[i] = (dU3dt[i]+pdU3dt[i])/2;
+    //人工粘性
+    U1[i] = U1[i] + dU1dtav[i] * deltaT + (Cx * abs(pp[i+1]-2*pp[i]+pp[i-1]) * (pU1[i+1]-2*pU1[i]+pU1[i-1])/(pp[i+1]+2*pp[i]+pp[i-1]));
+    U2[i] = U2[i] + dU2dtav[i] * deltaT + (Cx * abs(pp[i+1]-2*pp[i]+pp[i-1]) * (pU2[i+1]-2*pU2[i]+pU2[i-1])/(pp[i+1]+2*pp[i]+pp[i-1]));
+    U3[i] = U3[i] + dU3dtav[i] * deltaT + (Cx * abs(pp[i+1]-2*pp[i]+pp[i-1]) * (pU3[i+1]-2*pU3[i]+pU3[i-1])/(pp[i+1]+2*pp[i]+pp[i-1]));
+    }
+
+    //其他变量计算
+    for(int i=1;i < cellNum; i++){
+        rho[i] = U1[i]/A[i];
+        V[i] = U2[i]/U1[i];
+        T[i] = Tcal(U1[i],U2[i],U3[i]);
+        mdot[i] = U2[i];
+        Ma[i] = V[i] / sqrt(T[i]);
+        p[i] = rho[i]*T[i];
+    }
+
+    //头尾部
+    U2[0] = 2 * U2[1] - U2[2];
+    V[0] = U2[0]/U1[0];
+    U3[0] = U1[0] * ((1/(gamma-1.0))+(gamma * V[0]*V[0]/2));
+    mdot[0] = U2[0];
+    p[0] = rho[0]*T[0];
+    Ma[0] = V[0] / sqrt(T[0]);
+    U1[cellNum] = 2 * U1[cellNum-1] - U1[cellNum-2];
+    U2[cellNum] = 2 * U2[cellNum-1] - U2[cellNum-2];
+    U3[cellNum] = 2 * U3[cellNum-1] - U3[cellNum-2];
+    rho[cellNum] = U1[cellNum]/A[cellNum];
+    V[cellNum] = U2[cellNum]/U1[cellNum];
+    T[cellNum] = Tcal(U1[cellNum],U2[cellNum],U3[cellNum]);
+    mdot[cellNum] = U2[cellNum];
+    p[cellNum] = rho[cellNum]*T[cellNum];
+    Ma[cellNum] = V[cellNum] / sqrt(T[cellNum]);
+
+    if(j==0){
+       exportToFile("../output/Q3_iteration1.csv",X,A,rho,V,T,p,Ma,mdot,U1,U2,U3,cellNum);
+    }
+    }
+
+    //输出timesteps步后的数据
+    exportToFile("../output/Q3_final.csv",X,A,rho,V,T,p,Ma,mdot,U1,U2,U3,cellNum);
+
+    return 0;
+}
